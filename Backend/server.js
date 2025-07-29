74eac87afac05bc646f06442cefbec7f2580214e
Jamie <jamiesummeeow@gmail.com>
2025-07-29 07:51:29 -0400

    2 commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,24 +2,47 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import db from "./config/connection.js";
+import usersRouter from './routes/users.js';
+import postsRouter from './routes/posts.js';
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const PROD_URL = process.env.PROD_URL;
 
+// 1
+const whitelist = ["http://localhost:3000", PROD_URL];
+// 2
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+// 3
 app.use(cors());
+
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-db.connect();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
-import usersRouter from './routes/users.js';
-import postsRouter from './routes/posts.js';
 
-app.use('/api/users', usersRouter);
-app.use('/api/posts', postsRouter);
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+
+
 db.once("open", () => {
-  app.listen(process.env.PORT, () => console.log(`🌍 Now listening on localhost:${process.env.  PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
